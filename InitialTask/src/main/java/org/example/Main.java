1c2d2504be7b9f1cb6437daa626273f59d9474c5
Yura <y.vegner@g.nsu.ru>
2024-02-11 18:20:58 +0700

    final

--- InitialTask/src/main/java/org/example/Main.java
+++ InitialTask/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 class Main{
     public static void main(String[] args){
         Game game = new Game();
-        game.gameplay();
+        UserInterface info = new UserInterface();
+        info.information();
+        game.Gameplay();
     }
 }
--- InitialTask/src/main/java/org/example/UserInterface.java
+++ InitialTask/src/main/java/org/example/UserInterface.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class UserInterface {
+    public void information(){
+        System.out.println("You will need guess a four-digit number, good luck!!!");
+    }
 }
