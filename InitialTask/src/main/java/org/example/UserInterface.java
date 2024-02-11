9d4da53bae2ebbde26a001543a61cdef1c718cab
Yuri Wegner <131969860+Joerg1632@users.noreply.github.com>
2024-02-11 20:43:04 +0700

    Update UserInterface.java

--- InitialTask/src/main/java/org/example/UserInterface.java
+++ InitialTask/src/main/java/org/example/UserInterface.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public class UserInterface{
-
     public void startGame(){
         Game game = new Game();
         System.out.println("You will need guess a four-digit number, good luck!!!");
