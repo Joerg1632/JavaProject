e8cf8ad4b6382b695dbe67889be17ce41defda80
y.vegner <y.vegner@g.nsu.ru>
2024-03-11 01:04:23 +0700

    ChangeCommandInformation

--- CalculatorTask/src/main/java/org/example/CommandInformation.java
+++ CalculatorTask/src/main/java/org/example/CommandInformation.java
@@ -8,9 +8,20 @@ public class CommandInformation {
 
         for (String commandName : commandProperties.stringPropertyNames()) {
             String className = commandProperties.getProperty(commandName);
-            info.append(commandName).append(": ").append(className).append("\n");
+            info.append(formatCommandInfo(commandName)).append("\n");
         }
 
         return info.toString();
+    }
+
+    private String formatCommandInfo(String commandName) {
+        switch (commandName) {
+            case "PUSH":
+                return "Command " + commandName + " {num or var}: Takes a number or variable and pushes it onto the stack";
+            case "DEFINE":
+                return "Command " + commandName + " {var} {num}: Accepts a number and a variable, defining the variable to be the specified number";
+            default:
+                return "Command " + commandName + " {}: empty input(Work with stack)";
+        }
     }
 }
