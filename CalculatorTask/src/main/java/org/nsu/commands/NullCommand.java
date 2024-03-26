e0da5594e055f5f20291513a0fd222b3ef441bf4
Yuri Wegner <131969860+Joerg1632@users.noreply.github.com>
2024-03-26 10:04:47 +0700

    Delete CalculatorTask directory

--- CalculatorTask/src/main/java/org/nsu/commands/NullCommand.java
+++
@@ -1,8 +0,0 @@
-package org.nsu.commands;
-import org.nsu.data.ExecutionContext;
-
-public class NullCommand implements Command {
-    @Override
-    public void execute(ExecutionContext context) {
-    }
-}
